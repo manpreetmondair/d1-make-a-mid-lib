c6c6c6787adf3b4abd021ce474e0511a952f7964
MANPREET <mondair.manpreet@gmail.com>
2017-02-13 15:56:54 -0500

    Second Assignment

---
+++ js/madlib.js
@@ -0,0 +1,33 @@
+var firstName = 'Regan' ;
+var lastName = 'Sanner' ;
+var fullName = firstName.concat(lastName); 
+var action1 = 'lost';
+var noun1 = 'purse';
+var place = 'Mall' ;
+var adverb = 'When' ;
+var place1 = 'Home' ;
+var number = '5' ;
+var cards = 'mastercards' ;
+var noun2 = 'social' ;
+var expression = 'worried' ; 
+var conversation = 'Manager' ;
+var verb = 'happens' ;
+var action3 = 'back' ;
+var showproof = 'Driverlicence' ;
+var email = "mondair.manpreet@gmail.com";
+var time = '2 hours' ;
+var message =  'call' ;
+var number = '6' ;
+var action4 = 'get' ;
+var expression1 = 'Happily';
+var adverb1 = "After" ;
+
+
+var story = `One Day, ${fullName}, ${action1} her ${noun1} at ${place}, ${adverb} She get back ${place1} She Noticed that she had ${number} ${cards} in it Also noticed that, her ${noun2} was inside it. She was very ${expression}. She called to the ${conversation} and see what ${verb}, ${conversation} told her to call ${action3}. He also need ${showproof}  and After ${time} she received ${message} that He got one ${noun1} and She can show her ${showproof} and get her purse back. Immediately she went to the ${place} and show her ${showproof} and ${action4} her ${noun1} back Then She lives ${expression1} ever ${adverb1}.`;
+
+
+console.log (story);
+
+
+
+
